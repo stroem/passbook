d003423da803d129e92759673e63989b18d07246
stroem <jonathan.strom@gmail.com>
2016-09-12 17:16:28 +0200

    Error when marshaling beacons

--- beacon.go
+++ beacon.go
@@ -13,9 +13,9 @@ type Beacon struct {
 	RelevantText  string `json:"relevantText,omitempty"` // Text displayed on the lock screen when the pass is currently relevant.
 }
 
-func (b Beacon) MarshalJSON() ([]byte, error) {
+/*func (b Beacon) MarshalJSON() ([]byte, error) {
 	if b.ProximityUUID == "" {
 		return nil, errors.New("Unique identifier of a Bluetooth Low Energy location beacon must be set")
 	}
 	return json.Marshal(b)
-}
+}*/
